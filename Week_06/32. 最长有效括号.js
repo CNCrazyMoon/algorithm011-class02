f32459ad7572df75f931b68cf413d4c2e7abb003
song <luozxi@163.com>
2020-08-02 23:59:42 +0800

    week06

---
+++ Week_06/32. 最长有效括号.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const longestValidParentheses = (s) => {
+    let maxLen = 0;
+    const stack = [-1];
+    for (let i = 0; i < s.length; i++) {
+        const c = s[i];
+        if (c == '(') {
+            stack.push(i);
+            continue;
+        }
+        stack.pop();
+        if (stack.length == 0) {
+            stack.push(i);
+        } else {
+            maxLen = Math.max(maxLen, i - stack[stack.length - 1]);
+        }
+    }
+    return maxLen;
+};
